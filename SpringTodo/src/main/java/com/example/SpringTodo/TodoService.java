62e2a18693982f57cf30b1c99cd4e323139a7556
You, Jinho <jhyou@mokpo.ac.kr>
2023-08-15 02:53:21 +0900

    SpringTodo 추가

---
+++ SpringTodo/src/main/java/com/example/SpringTodo/TodoService.java
@@ -0,0 +1,32 @@
+package com.example.SpringTodo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    private final TodoMapper todoMapper;
+
+    @Autowired
+    public TodoService(TodoMapper todoMapper) {
+        this.todoMapper = todoMapper;
+    }
+
+    public List<Todo> getTodos() {
+        return todoMapper.findAll();
+    }
+
+    public void uploadTodos(List<Todo> todos) {
+        todoMapper.deleteAll(); // 기존 데이터를 삭제하고 새로운 데이터를 업로드
+        for (Todo todo : todos) {
+            todoMapper.insert(todo);
+        }
+    }
+
+    public void initTodos() {
+        todoMapper.deleteAll(); // 모든 데이터 삭제
+    }
+}
---
+++ SpringTodo/src/test/java/com/example/SpringTodo/TodoControllerTest.java
@@ -0,0 +1,59 @@
+package com.example.SpringTodo;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TodoControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private TodoMapper todoMapper;
+    
+    @InjectMocks
+    private TodoController todoController;
+
+    @InjectMocks
+    private TodoService todoService;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(todoController).build();
+
+        // todoController에 todoService를 주입
+        ReflectionTestUtils.setField(todoController, "todoService", todoService);
+        
+        Todo todo1 = new Todo("Task1", "2023-08-14", false);
+        Todo todo2 = new Todo("Task2", "2023-08-15", true);
+        List<Todo> todos = Arrays.asList(todo1, todo2);
+
+        // todoMapper.findAll() 호출을 흉내냄. 리턴 값을 todos로 함
+        when(todoMapper.findAll()).thenReturn(todos);
+    }
+
+    @Test
+    public void testDownloadTodos() throws Exception {
+        mockMvc.perform(get("/todo/download"))
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$", hasSize(2)))
+               .andExpect(jsonPath("$[0].todo").value("Task1"))
+               .andExpect(jsonPath("$[1].todo").value("Task2"));
+    }
+}
