90344d4042b0ad70446912ecb81d415595a09b07
You, Jinho <jhyou@mokpo.ac.kr>
2023-08-15 15:09:41 +0900

    JUnit5 Unit Test 추가

--- SpringTodo/src/test/java/com/example/SpringTodo/TodoControllerTest.java
+++ SpringTodo/src/test/java/com/example/SpringTodo/TodoControllerTest.java
@@ -9,37 +9,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(TestResultLogger.class)
+@ExtendWith(MockitoExtension.class)
 public class TodoControllerTest {
 
     private MockMvc mockMvc;
 
     @Mock
     private TodoMapper todoMapper;
-    
+
     @InjectMocks
     private TodoController todoController;
 
     @InjectMocks
     private TodoService todoService;
 
-    @Before
+    @BeforeEach
     public void setup() {
         this.mockMvc = MockMvcBuilders.standaloneSetup(todoController).build();
 
         // todoController에 todoService를 주입
         ReflectionTestUtils.setField(todoController, "todoService", todoService);
-        
+
         Todo todo1 = new Todo("Task1", "2023-08-14", false);
         Todo todo2 = new Todo("Task2", "2023-08-15", true);
         List<Todo> todos = Arrays.asList(todo1, todo2);
